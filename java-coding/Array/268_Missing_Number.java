371fe577cd72da1b0e42b398e853f13f19192984
hqcoder05 <hoangquocfw@gmail.com>
2025-12-24 16:10:43 +0700

    math solutions

--- java-coding/Array/268_Missing_Number.java
+++ java-coding/Array/268_Missing_Number.java
@@ -1,10 +1,8 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int expect = 0;
         int current = 0;
-        for (int i = 0; i <= nums.length; i++) {
-            expect += i;
-        }
+        int n = nums.length;
+        int expect = n * (n + 1) / 2;
         for (int num : nums) {
             current += num;
         }
