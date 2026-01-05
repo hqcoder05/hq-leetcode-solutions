d86a0984ee7c644fdfdc0697ab97d6facd57fa5c
hqcoder05 <hoangquocfw@gmail.com>
2026-01-05 21:12:04 +0700

    solutions

---
+++ java-coding/Two_Pointers/011_Container_With_Most_Water.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+        while (left <= right) {
+            int Area = (right - left) * Math.min(height[left], height[right]);
+            maxArea = Math.max(maxArea, Area);
+            if (height[left] <= height[right]) left++;
+            else right--;
+        }
+        return maxArea;
+    }
+}
